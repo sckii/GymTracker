4a3b95f52d634537015816ecb89050cee95c7bfa
Samuel Rodrigues <samur489@gmail.com>
2026-01-23 00:11:19 -0300

    Supbase hardcoded env

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://aparezodtgmmhflhkzpg.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_LrXYgALCh74mNJ2qlA3CxQ_QfSsCKM2';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
